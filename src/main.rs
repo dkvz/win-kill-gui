805f13bd74029beca05accf527dffbc6fb7c0abd
dkvz <github@dkvz.eu>
2021-05-04 13:39:46 +0200

    Initial

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use nwg::{self, NativeUi};
+mod main_window;
+use main_window::MainWindow;
+
+fn main() {
+  nwg::init().expect("Failed to init Native Windows GUI");
+  nwg::Font::set_global_family("Segoe UI").expect("Failed to set default font");
+
+  let _app = MainWindow::build_ui(Default::default())
+    .expect("Failed to build UI");
+
+  nwg::dispatch_thread_events();
+}
